50e8e87b1204ae62844d14015d4cb53febe72025
NineTwoProgramming <61386052+NineTwoProgramming@users.noreply.github.com>
2023-01-02 16:08:25 -0500

    Added comments

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,41 +1,43 @@
+// Global Variables
 var timerEl = document.querySelector('#timer');
 var buttonEl = document.querySelector('#startGame');
 var question = document.querySelector('#questions');
 var answers = document.querySelector('#answers');
 var questionResponse = document.querySelector('#questionResponse');
-var showScore = document.querySelector('#showScore');
 var questionCounter = 0;
 var timeLeft;
 var timeInterval;
 
+// Starts the timer and then calls the generateQuestions function after a delay
 function playGame() {
-    timeLeft = 16;
-  
-      timeInterval = setInterval(function () {
-      if (timeLeft >= 1) {
-        timeLeft--;
-        timerEl.textContent = 'Time: ' + timeLeft;
-      } else {
-        clearInterval(timeInterval);
-        changeQuestion()
-      }
-    }, 1000);
+  timeLeft = 16;
 
-    buttonEl.style.display = 'none';
+  timeInterval = setInterval(function () {
+    if (timeLeft >= 1) {
+      timeLeft--;
+      timerEl.textContent = 'Time: ' + timeLeft;
+    } else {
+      clearInterval(timeInterval);
+      changeQuestion()
+    }
+  }, 1000);
 
-    setTimeout(generateQuestions(questionCounter), 1000);
+  buttonEl.style.display = 'none';
+
+  setTimeout(generateQuestions(questionCounter), 1000);
 }
 
-function generateQuestions (x) {
-  if (questionCounter >= 3){
+// Generates the questions and options based off the questionCounter indexed number
+function generateQuestions(x) {
+  if (questionCounter >= 3) {
     return;
   }
 
   var popQuestion = quizInformation[x].question;
-  var popAnswer = '<p class="choice choiceHover">' + quizInformation[x].answers[0] + '</p>'
-                + '<p class="choice choiceHover">' + quizInformation[x].answers[1] + '</p>'
-                + '<p class="choice choiceHover">' + quizInformation[x].answers[2] + '</p>'
-                + '<p class="choice choiceHover">' + quizInformation[x].answers[3] + '</p>';
+  var popAnswer = '<p class="choice choiceHover">' + quizInformation[x].answers[0] + '</p>' +
+    '<p class="choice choiceHover">' + quizInformation[x].answers[1] + '</p>' +
+    '<p class="choice choiceHover">' + quizInformation[x].answers[2] + '</p>' +
+    '<p class="choice choiceHover">' + quizInformation[x].answers[3] + '</p>';
 
   question.textContent = popQuestion;
   answers.innerHTML = popAnswer;
@@ -47,7 +49,8 @@ function generateQuestions (x) {
   }
 }
 
-function chosenAnswer () {
+// Checks if the answer, chosen by the user, is correct or not
+function chosenAnswer() {
   if (questionCounter >= 3) {
     return;
   }
@@ -58,20 +61,21 @@ function chosenAnswer () {
   if (userChoice == correctAnswer) {
     questionResponse.textContent = 'Correct!';
     setTimeout(changeQuestion, 500);
-  }else {
+  } else {
     questionResponse.textContent = 'Wrong!';
     setTimeout(changeQuestion, 500);
     timeLeft -= 5;
   }
 }
 
+// Changes the quesion after each answer is chosen and ends game if time runs out
 function changeQuestion() {
   questionCounter++;
 
-  if (timeLeft <= 0){
+  if (timeLeft <= 0) {
     question.textContent = 'You lost!';
-    answers.innerHTML = '<p>Would you like to play again?</p>'
-    + '<form action="./index.html"><input type="submit" value="Play Again"></form>';
+    answers.innerHTML = '<p>Would you like to play again?</p>' +
+      '<form action="./index.html"><input type="submit" value="Play Again"></form>';
     questionResponse.textContent = '';
 
     return;
@@ -82,34 +86,36 @@ function changeQuestion() {
     endGame();
   };
 
-  generateQuestions(questionCounter); 
+  generateQuestions(questionCounter);
   questionResponse.textContent = '';
 }
 
+// Ends the game by showing the final score of the user and asks for their initials to save to local storage
 function endGame() {
   var finalScore = document.querySelector('#showScore');
   timerEl.textContent = '';
   question.textContent = 'Final Score!';
   answers.textContent = '';
-  finalScore.innerHTML = '<h1>You had ' + Math.ceil(timeLeft) + ' seconds left.</h1>'
-                        + '<h3>Enter initials to save score:</h3>';
+  finalScore.innerHTML = '<h1>You had ' + Math.ceil(timeLeft) + ' seconds left.</h1>' +
+    '<h3>Enter initials to save score:</h3>';
 
   var initials = document.querySelector('#form');
-  initials.innerHTML = '<input type="text" id="initials">'
-                     + '<button id="saveScore">Test</button>';
+  initials.innerHTML = '<input type="text" id="initials">' +
+    '<button id="saveScore">Submit</button>';
 
-  var initialSubmit = document.querySelector('#saveScore');                       
+  var initialSubmit = document.querySelector('#saveScore');
 
-  initialSubmit.onclick = function() {
-  var initialsText = document.querySelector('#initials').value;
+  initialSubmit.onclick = function () {
+    var initialsText = document.querySelector('#initials').value;
 
-  var results = {
-    initials: initialsText,
-    score: timeLeft
-  };
-                      
-  localStorage.setItem(localStorage.length + 1, JSON.stringify(results));
+    var results = {
+      initials: initialsText,
+      score: timeLeft
+    };
+
+    localStorage.setItem(localStorage.length + 1, JSON.stringify(results));
   }
 }
 
+// Adds a click event listener to the buttonEl and executes playGame
 buttonEl.addEventListener('click', playGame);
